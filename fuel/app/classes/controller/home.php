5dd9733dbd127c1f709614a2901d0a580332f7d0
maxxyPhp <maxxy62980@hotmail.fr>
2014-09-23 16:51:58 +0200

    [NEW] Mise en place de la home

--- fuel/app/classes/controller/home.php
+++ fuel/app/classes/controller/home.php
@@ -10,7 +10,18 @@ class Controller_Home extends Controller
 	 */
 	public function action_index()
 	{
-		return Response::forge(View::forge('home/index'));
+		// create the layout view
+        $view = View::forge('layout');
+
+        //local view variables, lazy rendering
+        $view->head = View::forge('home/head', array('title' => 'FIFAGOAL', 'description' => 'Application de gestion et de report de matchs joués sur le jeu vidéo de football FIFA'));
+        $view->header = View::forge('home/header', array('site_title' => 'My Website'));
+        $view->content = View::forge('home/content', array('username' => 'Joe14', 'title' => 'Home'));
+        $view->footer = View::forge('home/footer', array('site_title' => 'My Website'));
+
+        // return the view object to the Request
+        return $view;
+		// return Response::forge(View::forge('home/index'));
 	}
 
 	/**
---
+++ fuel/app/views/home/head.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title><?php echo $title; ?></title>
+<meta name="description" content="<?php echo $description; ?>"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<?= \Asset::css('bootstrap/bootstrap.min.css'); ?>
+<?= \Asset::css('font-awesome/css/font-awesome.min.css') ?>
+
+<?= \Asset::js('jquery/jquery-2.1.1.min.js') ?>
+<?= \Asset::js('bootstrap/bootstrap.min.js') ?>
