90e6f3ae3d56b936ba28d048e62002d4d0b9a525
maxxyPhp <maxxy62980@hotmail.fr>
2014-09-24 22:00:42 +0200

    [NEW] Ajout du CSS

--- fuel/app/views/home/head.php
+++ fuel/app/views/home/head.php
@@ -8,6 +8,7 @@
   <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 
+<?= \Asset::css('style.css'); ?>
 <?= \Asset::css('bootstrap/bootstrap.min.css'); ?>
 <?= \Asset::css('font-awesome/css/font-awesome.min.css') ?>
 
