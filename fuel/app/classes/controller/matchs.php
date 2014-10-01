3268389c1e71c89b6af36d5009354e86d1a1cd32
maxxyPhp <maxxy62980@hotmail.fr>
2014-10-01 23:31:45 +0200

    [NEW]Ajouter un rapport de match

--- fuel/app/classes/controller/matchs.php
+++ fuel/app/classes/controller/matchs.php
@@ -20,7 +20,7 @@ class Controller_Matchs extends \Controller_Front
 				$defis = \Model_Defis::forge();
 				$defis->id_joueur_defieur = \Auth::get('id');
 				$defis->id_joueur_defier = $defier->id;
-				$defis->status_demande = $status->code;
+				$defis->status_demande = $status->id;
 				$defis->save();
 
 				return json_encode('OK');
@@ -29,7 +29,111 @@ class Controller_Matchs extends \Controller_Front
 	}
 
 	public function action_index (){
-		return $this->view('matchs/index', array('users' => $array));
+		//return $this->view('matchs/index', array('users' => $array));
+	}
+
+	public function action_add (){
+		$this->verifAutorisation();
+
+		if (\Input::post('add')){
+			// METTRE ID DEFI DANS FORM
+			// VERIFIER ID JOUERS AVEC CEUX DU DEFI
+			if (!is_numeric(\Input::post('joueur1')) || !is_numeric(\Input::post('joueur2')) || !is_numeric(\Input::post('defi'))){
+				\Messages::error('Problèmes avec les joueurs');
+				\Response::redirect('/defis');
+			}
+
+			$defi = \Model_Defis::find(\Input::post('defi'));
+			if (empty($defi)){
+				\Messages::error('Problème de défi');
+				\Response::redirect('/defis');
+			}
+
+			$defieur = \Model\Auth_User::find(\Input::post('joueur1'));
+			if (empty($defieur)){
+				\Messages::error('Le joueur 1 n\'existe pas');
+				\Response::redirect('/defis');
+			}
+			$defier = \Model\Auth_User::find(\Input::post('joueur2'));
+			if (empty($defier)){
+				\Messages::error('Le joueur 2 n\'existe pas');
+				\Response::redirect('/defis');
+			}
+			
+			if ($defi->id_joueur_defieur != $defieur->id || $defi->id_joueur_defier != $defier->id){
+				\Messages::error('Les joueurs ne correspondent pas au défi initial');
+				\Response::redirect('/defis');
+			}
+
+			$equipe1 = \Model_Equipe::find(\Input::post('id_equipe_defieur'));
+			if (empty($equipe1)){
+				\Messages::error('L\'équipe de '.$defieur->username.' n\'existe pas');
+				\Response::redirect('/defis');
+			}
+
+			$equipe2 = \Model_Equipe::find(\Input::post('id_equipe_defier'));
+			if (empty($equipe2)){
+				\Messages::error('L\'équipe de '.$defier->username.' n\'existe pas');
+				\Response::redirect('/defis');
+			}
+
+			$match = \Model_Matchs::forge();
+			$match->id_joueur1 = $defieur->id;
+			$match->id_joueur2 = $defier->id;
+			$match->id_equipe1 = $equipe1->id;
+			$match->id_equipe2 = $equipe2->id;
+			$match->score_joueur1 = \Input::post('score_joueur_1');
+			$match->score_joueur2 = \Input::post('score_joueur_2');
+			$match->save();
+
+			$defi->id_match = $match->id;
+			if ($defi->save()){
+				\Messages::success('Le rapport du match a bien été enregistré. Votre adversaire recevra une notification pour le valider');
+				\Response::redirect('/defis');
+			}
+		}
+
+		if (!is_numeric(\Input::post('defi'))){
+			\Messages::error('Données erronées');
+			\Response::redirect('/defis');
+		}
+
+		/**
+		 *
+		 * ZONE USER
+		 *
+		 */
+		$defi = \Model_Defis::find(\Input::post('defi'));
+		if (empty($defi)){
+			\Messages::error('Ce défi n\'existe pas');
+			\Response::redirect('/defis');
+		}
+
+		$defieur = \Model\Auth_User::find($defi->id_joueur_defieur);
+		$defier = \Model\Auth_User::find($defi->id_joueur_defier);
+
+		$photo_defieur = \Model_Photousers::query()->where('id_users', '=', $defieur->id)->get();
+		(!empty($photo_defieur)) ? $photo_defieur = current($photo_defieur) : $photo_defieur = null;
+
+		$photo_defier = \Model_Photousers::query()->where('id_users', '=', $defier->id)->get();
+		(!empty($photo_defier)) ? $photo_defier = current($photo_defier) : $photo_defier = null;
+
+
+		/**
+		 *
+		 * ZONE FOOT
+		 *
+		 */
+		// Pays qui ont un championnat
+		$query = \DB::query('SELECT DISTINCT pays.nom, pays.id, drapeau FROM pays JOIN championnat ON championnat.id_pays = pays.id ORDER BY pays.nom')->as_object('Model_Pays')->execute();
+		$pays = array();
+		foreach ($query as $result){
+			$pays[] = $result;
+		}
+
+		$championnats = \Model_Championnat::find('all');
+
+		return $this->view('matchs/add', array('defi' => $defi, 'defieur' => $defieur, 'defier' => $defier, 'photo_defieur' => $photo_defieur, 'photo_defier' => $photo_defier, 'pays' => $pays, 'championnats' => $championnats));
 	}
 
 }
---
+++ fuel/app/views/matchs/add.php
@@ -0,0 +1,232 @@
+<div class="container">
+	<h1 class="center-block center">RAPPORT DE MATCH</h1>
+	<div class="row rapport-match">
+		<?= \Form::open(array('class' => 'form-horizontal')) ?>
+		<input type="hidden" name="defi" value="<?= $defi->id ?>">
+
+		<!-- DEFIEUR -->
+		<div class="col-md-4">
+			<img src="<?= \Uri::base() . \Config::get('users.photo.path') . $photo_defieur->photo ?>" alt="<?= $defieur->username ?>" class="img-thumbnail center-block img-profil-rapport animated fadeInUp" width="120px" />
+			<input type="hidden" name="joueur1" value="<?= $defieur->id ?>">
+			<div class="form-group animated fadeInUp">
+				<div class="col-sm-10">
+					<select id="form_championnat_defieur">
+						<option></option>
+						<?php foreach ($pays as $pay): ?>
+							<optgroup label="<?= $pay->nom ?>">
+							<?php foreach ($championnats as $championnat): ?>
+								<?php if ($championnat->id_pays == $pay->id): ?>
+									<option value="<?= $championnat->id ?>"><?= $championnat->nom ?></option>
+								<?php endif; ?>
+							<?php endforeach; ?>
+							</optgroup>
+						<?php endforeach; ?>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group" style="display:none;" id="div_equipes_defieur">
+				<div class="col-sm-10">
+					<select id="form_equipe_defieur" name="id_equipe_defieur">
+						<option></option>
+					</select>
+				</div>
+			</div>
+		</div>
+
+		<!-- SCORE -->
+		<div class="col-md-4">
+			<div class="row">
+				<div class="col-md-6 club club_defieur"></div>
+				<div class="col-md-6 club club_defier"></div>
+			</div>
+
+			<div class="score" style="display:none;">
+				<div class="row"><h2 class="center-block center">Score</h2></div>
+				<div class="row">
+					
+					<div class="col-md-6">
+						<input type="number" class="form-control" id="score_joueur1" name="score_joueur_1" min="0" max="20">
+					</div>
+					<div class="col-md-6">
+						<input type="number" class="form-control" id="score_joueur2" name="score_joueur_2" min="0" max="20">
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row valid_match">
+				<input type="submit" name="add" value="Valider le match" class="btn btn-primary btn-lg btn-block" disabled="disabled">
+			</div>
+		</div>
+
+
+		<!-- DEFIER -->
+		<div class="col-md-4">
+			<img src="<?= \Uri::base() . \Config::get('users.photo.path') . $photo_defier->photo ?>" alt="<?= $defier->username ?>" class="img-thumbnail center-block img-profil-rapport animated fadeInUp" width="120px" />
+			<input type="hidden" name="joueur2" value="<?= $defier->id ?>">
+			<div class="form-group animated fadeInUp">
+				<div class="col-sm-10">
+					<select id="form_championnat_defier">
+						<option></option>
+						<?php foreach ($pays as $pay): ?>
+							<optgroup label="<?= $pay->nom ?>">
+							<?php foreach ($championnats as $championnat): ?>
+								<?php if ($championnat->id_pays == $pay->id): ?>
+									<option value="<?= $championnat->id ?>"><?= $championnat->nom ?></option>
+								<?php endif; ?>
+							<?php endforeach; ?>
+							</optgroup>
+						<?php endforeach; ?>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group" style="display:none;" id="div_equipes_defier">
+				<div class="col-sm-10">
+					<select id="form_equipe_defier" name="id_equipe_defier">
+						<option></option>
+					</select>
+				</div>
+			</div>
+		</div>
+		<?= \Form::close(); ?>
+	</div>
+</div>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#form_championnat_defieur').select2({
+			placeholder: "Selectionnez un championnat",
+			allowClear: true,
+			width: '300px'
+		});
+
+		$('#form_equipe_defieur').select2({
+			placeholder: "Selectionnez une équipe",
+			allowClear: true,
+			width: '300px'
+		});
+
+		$('#form_championnat_defieur').on('change', function(){
+			id_championnat = $(this).val();
+			afficherEquipes(id_championnat, $('#form_equipe_defieur'), $('#div_equipes_defieur'));
+		});
+
+		$('#form_championnat_defier').select2({
+			placeholder: "Selectionnez un championnat",
+			allowClear: true,
+			width: '300px'
+		});
+
+		$('#form_equipe_defier').select2({
+			placeholder: "Selectionnez une équipe",
+			allowClear: true,
+			width: '300px'
+		});
+
+		$('#form_championnat_defier').on('change', function(){
+			id_championnat = $(this).val();
+			afficherEquipes(id_championnat, $('#form_equipe_defier'), $('#div_equipes_defier'));
+		});
+
+		/**
+		 * afficherEquipes
+		 * Affiche le nom des équipes d'un championnat
+		 *
+		 * @param int id_championnat
+		 * @param Noeud select : le select contenant le nom des équipes
+		 * @param Noeud afficher : la div contenant le select
+		 */
+		function afficherEquipes (id_championnat, select, afficher){
+			$.ajax({
+				url : window.location.origin + '/equipe/api/getEquipes.json',
+				data: 'id_championnat='+id_championnat,
+				type: 'get',
+				dataType: 'json',
+				success: function(data){
+					if (data != 'KO'){
+						select.html('');
+						equipe = data;
+						for (var i in equipe){
+							select.append('<option value="'+equipe[i]['id']+'">'+equipe[i]['nom']+'</option>');
+						}
+						afficher.addClass('animated fadeInUp').show();
+					}
+				},
+				error: function(){
+					alert('Une erreur est survenue');
+				}
+			});
+		}
+
+
+		$('#form_equipe_defieur').on('change', function(){
+			clickEquipe ($(this).val(), $('.club_defieur'), 'logo_club_defieur');
+		});
+
+		$('#form_equipe_defier').on('change', function(){
+			clickEquipe ($(this).val(), $('.club_defier'), 'logo_club_defier');
+		});
+
+		/**
+		 * ClickEquipe
+		 * Affiche le logo de l'équipe sélectionnée
+		 *
+		 * @param id_equipe : l'id de l'équipe sélectionnée
+		 * @param div_equipe : la div où l'image doit apparaître
+		 * @param classImg : la classe donnée à l'image
+		 */
+		function clickEquipe (id_equipe, div_equipe, classImg){
+			$('.'+classImg).remove();
+			$.ajax({
+				url: window.location.origin + '/equipe/api/getEquipe.json',
+				data: 'id_equipe=' + id_equipe,
+				dataType: 'json',
+				type: 'get',
+				success : function(data){
+					div_equipe.append(
+						'<img src="'+window.location.origin+'/upload/equipes/'+data[7]+'/'+data[2]+'" alt="'+data[0]+'" width="100px" class="'+classImg+'" />'
+					);
+
+					if ($('#form_equipe_defieur').val() != 0 && $('#form_equipe_defier').val() != 0){
+						$('.score').show();
+					}
+				},
+				error: function(){
+					alert('Une erreur est survenue');
+				},
+			});
+		}
+
+		$('#score_joueur1').on('keyup', function(){
+			actionnerValidation();
+		});
+
+		$('#score_joueur1').on('blur', function(){
+			actionnerValidation();
+		});
+
+		$('#score_joueur1').on('click', function(){
+			actionnerValidation();
+		});
+
+		$('#score_joueur2').on('keyup', function(){
+			actionnerValidation();
+		});
+
+		$('#score_joueur2').on('blur', function(){
+			actionnerValidation();
+		});
+
+		$('#score_joueur2').on('click', function(){
+			actionnerValidation();
+		});
+
+		function actionnerValidation(){
+			if ($('#score_joueur1').val() != '' && $('#score_joueur2').val() != ''){
+				$('input:submit').attr('disabled', false);
+			}
+		}
+	});
+</script>
