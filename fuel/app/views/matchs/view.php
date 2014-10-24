11078ed16d9f6bbf36bc2978ebe6f6eb892fc8e0
maxxyPhp <maxxy62980@hotmail.fr>
2014-10-24 21:40:54 +0200

    [UPDATE] Match view : Réseaux sociaux que si valider

--- fuel/app/views/matchs/view.php
+++ fuel/app/views/matchs/view.php
@@ -5,7 +5,7 @@
 			<p>Il ne sera pas publié tant que les deux joueurs ne valident pas le rapport.</p>
 			<p>
 				<a class="btn btn-success btn-confirm" data-user="<?= \Auth::get('id') ?>" data-match="<?= $match->id ?>"><i class="fa fa-check"></i> Je valide</a>
-				<a href="/matchs/modif/<?= $match->id ?>" class="btn btn-danger btn-refuse"><i class="fa fa-exclamation"></i> Je veux modifier ce rapport, car il y a une erreur</a>
+				<a href="/matchs/modif/<?= $match->id ?>" class="btn btn-danger btn-refuse" data-loading-text="Chargement..."><i class="fa fa-exclamation"></i> Je veux modifier ce rapport, car il y a une erreur</a>
 			</p>
 		</div>
 	<?php elseif (!$match_valider): ?>
@@ -19,25 +19,27 @@
 		</div>
 
 		<div class="panel-body">
-			<div id="fb-root"></div>
-			<div class="row">
-				<div class="col-md-2">
-					<?php if (!$jaime): ?>
-						<a class="btn btn-default btn-like" data-match="<?= $match->id ?>" style="margin-left:50px;"><i class="fa fa-thumbs-o-up"></i> J'applaudis</a>
-					<?php else: ?>
-						<a class="btn btn-default btn-like" data-match="<?= $match->id ?>" disabled="disabled" style="margin-left:50px;"><i class="fa fa-thumbs-o-up"></i> J'applaudis</a>
-					<?php endif; ?>
-				</div>
-				<div class="col-md-10 nb_like" data-like="<?= count($match->like) ?>">
-					<a class="btn-jaime" data-toggle="modal" data-target="#myModal"><i class="fa fa-thumbs-up"></i> <?= count($match->like) ?><?php if ($jaime): ?>  Vous applaudissez.<?php endif; ?></a>
-					<a href="#panel-commentaires" style="margin-left:30px;"><i class="fa fa-comments"></i> <?= count($commentaires) ?></a>
-					<div style="margin-left:20px;" class="fb-share-button" data-href="<?= \Uri::base() ?>matchs/view/<?= $match->id ?>" data-layout="button_count"></div>
-					<div class="bouton-twitter">
-						<a href="https://twitter.com/share" class="twitter-share-button" data-lang="fr" data-hashtags="FIFAGOAL">Tweeter</a>
+			<?php if ($match->defi->match_valider1 == 1 && $match->defi->match_valider2 == 1): ?>
+				<div id="fb-root"></div>
+				<div class="row">
+					<div class="col-md-2">
+						<?php if (!$jaime): ?>
+							<a class="btn btn-default btn-like" data-match="<?= $match->id ?>" style="margin-left:50px;"><i class="fa fa-thumbs-o-up"></i> J'applaudis</a>
+						<?php else: ?>
+							<a class="btn btn-default btn-like" data-match="<?= $match->id ?>" disabled="disabled" style="margin-left:50px;"><i class="fa fa-thumbs-o-up"></i> J'applaudis</a>
+						<?php endif; ?>
+					</div>
+					<div class="col-md-10 nb_like" data-like="<?= count($match->like) ?>">
+						<a class="btn-jaime" data-toggle="modal" data-target="#myModal"><i class="fa fa-thumbs-up"></i> <?= count($match->like) ?><?php if ($jaime): ?>  Vous applaudissez.<?php endif; ?></a>
+						<a href="#panel-commentaires" style="margin-left:30px;"><i class="fa fa-comments"></i> <?= count($commentaires) ?></a>
+						<div style="margin-left:20px;" class="fb-share-button" data-href="<?= \Uri::base() ?>matchs/view/<?= $match->id ?>" data-layout="button_count"></div>
+						<div class="bouton-twitter">
+							<a href="https://twitter.com/share" class="twitter-share-button" data-lang="fr" data-hashtags="FIFAGOAL">Tweeter</a>
+						</div>
 					</div>
 				</div>
-			</div>
-			<hr>
+				<hr>
+			<?php endif; ?>
 
 			<div class="row">
 				<!-- DEFIEUR -->
@@ -498,6 +500,10 @@
 				},
 			});
 		});
+
+		$('.btn-refuse').on('click', function(){
+			$(this).button('loading');
+		})
 	});
 </script>
 <script type="text/javascript">
