0644bba6e82c97b7064bbec65ca5c9b774d76110
maxxyPhp <maxxy62980@hotmail.fr>
2014-11-05 18:40:06 +0100

    [NEW] Ajout du favicon

--- fuel/app/views/home/head.php
+++ fuel/app/views/home/head.php
@@ -7,6 +7,7 @@
 <?php endif; ?>
 <meta name="description" content="<?php echo $description; ?>"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<link rel="icon"  type="image/png" href="<?= \Uri::base() ?>assets/img/favicon.png" />
 
 <!--[if lt IE 9]>
   <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
