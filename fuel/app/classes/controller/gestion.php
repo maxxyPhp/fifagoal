bffd1143a9bc1d77607d5ea4875c5ea60c639593
maxxyPhp <maxxy62980@hotmail.fr>
2014-11-10 16:29:36 +0100

    [NEW] Ajout fonction Secure dans Controller Gestion

--- fuel/app/classes/controller/gestion.php
+++ fuel/app/classes/controller/gestion.php
@@ -100,4 +100,34 @@ class Controller_Gestion extends \Controller
 	 
 	    return $data;
 	}
+
+	/**
+	 * Secure
+	 * Sécurise les données entrantes d'un formulaire
+	 *
+	 * @param String $string
+	 * @return String $string
+	 */
+	public static function secure($string)
+	{
+		// On regarde si le type de string est un nombre entier (int)
+		if(ctype_digit($string))
+		{
+			$string = intval($string);
+		}
+		// Pour tous les autres types
+		else
+		{
+			$string = mysql_real_escape_string($string);
+			$string = addcslashes($string, '%_');
+		}
+			
+		return $string;
+
+	}
+	// Données sortantes
+	public static function html($string)
+	{
+		return htmlentities($string);
+	}
 }
